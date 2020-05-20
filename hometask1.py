f026838ef3d0b38f176de5e020560ef5cf0b8b91
lesiayanytska7 <65349630+lesiayanytska7@users.noreply.github.com>
2020-05-20 17:18:00 +0300

    Add files via upload

---
+++ hometask1.py
@@ -0,0 +1,25 @@
+"""
+Task 2
+Output question 'What is your name', 'How old are you?', 'Where do you live?'
+"""
+print('What is you name?') #output name
+name=input()
+print('Hello,' + name)
+
+print('How old are you?') #output age
+age=input()
+print('Your age is,' + age)
+
+print('Where do you live?') #output city
+city=input()
+print('You live in,' + city)
+"""
+Task 1
+Define variables a and b. Read values from console and calculate
+"""
+a=int(5) #input
+b=int(4)
+print('a+b=',a+b) #printing the sum
+print('a-b=',a-b)
+print('a*b=',a*b)
+print('a/b=',a/b)
